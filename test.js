f9b87356689f0332bae5ef661e6e4b2848ff4c33
lambert5886 <lambert5886@126.com>
2019-01-26 17:35:47 +0800

    添加 promise 错误处理

--- test.js
+++ test.js
@@ -4,24 +4,42 @@
 // 常规函数;
 function defs(data){
     console.log('from defs >>> ', data);
-    return data;
+    if(data){
+        return data;
+    }else{
+        
+    }
+    
 }
 // 包装函数
 function wrapPro(fn){
-    let _s = Promise.resolve(fn);
+    let _s = new Promise( (resolve, reject) => {
+        if(fn){
+            resolve(fn);
+        }else{
+            reject('error >>> ||| ');
+        }
+    });
     _s.then( (result) => {
         console.log('from wrapPro >>> ', result);
+        
+    }, (error) => {
+        
     });
     return _s;
 }
 
-let wrap1 = wrapPro(defs('hello!')).then( (result) => {
+let wrap1 = wrapPro(defs()).then( (result) => {
     console.log(' from wraped > ', result)
     return wrapPro(result)
 });
 
 wrapPro(wrap1).then( res => {
     console.log('outwrap > ', res)
+}).catch( (res) => {
+    console.log('error ||| >> ', res)
 })
+
+
 
  
